2f4edeef2013686463e62777c5d5d035a2358d6b
Jeremy Ellis <keyfreemusic@gmail.com>
2016-01-06 19:40:16 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -127,9 +127,9 @@ git clone --recurse-submodules https://github.com/tensorflow/tensorflow
 echo "Now download the image sets"
 
 
-$ wget https://storage.googleapis.com/download.tensorflow.org/models/inception5h.zip -O /tmp/inception5h.zip
+wget https://storage.googleapis.com/download.tensorflow.org/models/inception5h.zip -O /tmp/inception5h.zip
 
-$ unzip /tmp/inception5h.zip -d /home/ubuntu/workspace/bazel/examples/android/assets/
+unzip /tmp/inception5h.zip -d /home/ubuntu/workspace/bazel/examples/android/assets/
 
 
 
