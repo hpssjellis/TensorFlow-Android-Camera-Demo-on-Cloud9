387b1b4c8497b8cde1207dea21a1097ad083afa0
Jeremy Ellis <keyfreemusic@gmail.com>
2016-01-06 19:15:42 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -19,6 +19,31 @@ echo "--------------------------------------------------------------"
 echo ". "
 
 
+echo "Now checking if java is installed"
+
+java -version
+
+echo "--------------------------------------------------------------"
+echo ". "
+
+echo "Now checking if java is installed"
+
+java -version
+
+echo "--------------------------------------------------------------"
+echo ". "
+
+
+
+echo "Now checking if a jdk is installed"
+
+readlink -f $(which java)
+
+echo "--------------------------------------------------------------"
+echo ". "
+
+
+
 echo "next list this directory"
 pwd
 ls -lah  
@@ -28,14 +53,11 @@ echo "Some other useful commands are cd     cd ..      dir    ls     pwd     "
 echo "."
 
 
-echo "--------------------------------------------------------------"
-echo "Install TensorFlow  "
-echo "."
 
-git clone --recurse-submodules https://github.com/tensorflow/tensorflow
+echo "Now trying lots of installs"
+read -p "Press [Enter] key to start backup..."
 
 
-echo "TensorFlow installed"
 
 
 
@@ -50,11 +72,47 @@ echo "Installing Java"
 
 sudo add-apt-repository ppa:webupd8team/java
 
-#sudo apt-get update
+sudo apt-get update
 
 sudo apt-get install oracle-java8-installer
 
+sudo apt-get install openjdk-8-jdk
+
+
+
+echo "Again checking if a jdk is installed"
+
+java -version
+
+readlink -f $(which java)
+
 echo "--------------------------------------------------------------"
+echo ". "
+
+
+
+
+echo "Now trying Bazel"
+read -p "Press [Enter] key to start backup..."
+
+
+
+#wget --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u20-b26/jdk-8u20-linux-x64.tar.gz
+
+
+
+git clone https://github.com/bazelbuild/bazel.git
+
+
+cd bazel
+run compile.sh
+
+cd ..
+
+
+
+
+echo "-------------DONE-------------------------------------------------"
 echo ". "
 
 
@@ -85,9 +143,9 @@ echo ". "
 
 
 
-# delte the re
 
-echo "Activate the environemtn use deactivate to get your cursor back"
+
+echo "Activate the environment use deactivate to get your cursor back"
 source ~/virtual-tf/bin/activate 
 
 
@@ -96,7 +154,7 @@ echo ". "
 
 echo "Now intall tensorFlow into the enviroment"
 
-pip install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.5.0-cp27-none-linux_x86_64.whl
+#pip install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.5.0-cp27-none-linux_x86_64.whl
 
 echo "--------------------------------------------------------------"
 echo ". "
@@ -104,9 +162,9 @@ echo ". "
 echo "Unfortunately on cloud 9 Pip hides the TensorFlow folder so lets clone it for our use"
 echo "Kind of wasteful but it isn't on our computer anyway"
 
-deactivate
+#deactivate
 
-git clone --recurse-submodules https://github.com/tensorflow/tensorflow
+#git clone --recurse-submodules https://github.com/tensorflow/tensorflow
 
 
 echo "--------------------------------------------------------------"
