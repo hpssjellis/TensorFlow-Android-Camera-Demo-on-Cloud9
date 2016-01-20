c6701de3ba1ae00877e393589f74be3eb080ed77
Jeremy Ellis <keyfreemusic@gmail.com>
2016-01-20 14:38:05 -0800

    Create a05-rocksetta-paths.sh

---
+++ a05-rocksetta-paths.sh
@@ -0,0 +1,10 @@
+#!/bin/bash  
+
+#  only need to run this script with the command (do not type the #)
+#  bash a05-rocksetta-paths.sh
+# best in cloud 9 to just right click this file and select run
+
+
+
+
+echo "Lets try to move the folders and setup PATHS"
