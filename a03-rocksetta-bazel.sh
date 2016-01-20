32819e211953690aaee07852e045e074e37f82f8
Jeremy Ellis <keyfreemusic@gmail.com>
2016-01-20 14:35:17 -0800

    Create a03-rocksetta-bazel.sh

---
+++ a03-rocksetta-bazel.sh
@@ -0,0 +1,41 @@
+#!/bin/bash  
+
+#  only need to run this script with the command (do not type the #)
+#  bash a03-rocksetta-bazel.sh
+# best in cloud 9 to just right click this file and select run
+
+
+
+echo "Now trying Bazel"
+read -p "Press [Enter] key to start "
+
+
+#https://github.com/bazelbuild/bazel/releases/download/0.1.2/bazel-0.1.2-jdk7-installer-linux-x86_64.sh
+
+
+
+#wget --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u20-b26/jdk-8u20-linux-x64.tar.gz
+
+
+
+#git clone https://github.com/bazelbuild/bazel.git
+
+
+#cd bazel
+#bash compile.sh
+
+#cd ..
+
+
+
+
+#https://github.com/bazelbuild/bazel/releases/download/0.1.2/bazel-0.1.2-jdk7-installer-linux-x86_64.sh
+wget https://github.com/bazelbuild/bazel/releases/download/0.1.2/bazel-0.1.2-jdk7-installer-linux-x86_64.sh -O /home/ubuntu/workspace/bazel
+
+cd bazel
+bash bazel-0.1.2-jdk7-installer-linux-x86_64.sh
+cd ..
+
+
+echo "----------------Bazel Done----------------------------------------------"
+echo ". "
