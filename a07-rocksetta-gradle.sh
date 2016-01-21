4b0149f8500df577b2254a67fc36d7e51244770b
Jeremy Ellis <keyfreemusic@gmail.com>
2016-01-20 23:16:35 -0800

    Create a07-rocksetta-gradle.sh

---
+++ a07-rocksetta-gradle.sh
@@ -0,0 +1,12 @@
+#!/bin/bash  
+
+#  only need to run this script with the command (do not type the #)
+#  bash a07-rocksetta-gradle.sh
+# best in cloud 9 to just right click this file and select run
+
+
+#RUN cd /usr/local && \
+#    curl -L https://services.gradle.org/distributions/gradle-2.5-bin.zip -o gradle-2.5-bin.zip && \
+#    unzip gradle-2.5-bin.zip
+
+
