1cfde8d9c96dade7f0ed676f37d5ad08ff9da0e4
Jeremy Ellis <keyfreemusic@gmail.com>
2016-01-20 23:15:12 -0800

    Create a06-rocksetta-setup-android.sh

---
+++ a06-rocksetta-setup-android.sh
@@ -0,0 +1,21 @@
+#!/bin/bash  
+
+#  only need to run this script with the command (do not type the #)
+#  bash a06-rocksetta-setup-android.sh
+# best in cloud 9 to just right click this file and select run
+
+
+
+#"android list sdk" will connect to the remote repository and list all the packages available with an index number.
+#"android update sdk --no-ui" accepts a "--filter" argument that can take a package index, or a category name.
+
+
+#RUN \  a docker command
+#    echo y | /usr/local/android-sdk-linux/tools/android update sdk --filter tools --no-ui --force -a && \
+#    echo y | /usr/local/android-sdk-linux/tools/android update sdk --filter platform-tools --no-ui --force -a && \
+#    echo y | /usr/local/android-sdk-linux/tools/android update sdk --filter android-19 --no-ui --force -a && \
+#    echo y | /usr/local/android-sdk-linux/tools/android update sdk --filter android-21 --no-ui --force -a && \
+#    echo y | /usr/local/android-sdk-linux/tools/android update sdk --filter android-22 --no-ui --force -a && \
+ #   echo y | /usr/local/android-sdk-linux/tools/android update sdk --filter android-23 --no-ui --force -a && \
+#    echo y | /usr/local/android-sdk-linux/tools/android update sdk --filter extra --no-ui --force -a && \
+#    echo y | /usr/local/android-sdk-linux/tools/android update sdk --all --filter build-tools-23.0.2 --no-ui --force
