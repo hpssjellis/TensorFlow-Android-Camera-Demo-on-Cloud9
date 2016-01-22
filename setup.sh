638ea92ac40c1db2b8cf2eec20df4129395d4941
Jeremy Ellis <keyfreemusic@gmail.com>
2016-01-21 21:29:20 -0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash  
+
+#  only need to run this script with the command (do not type the #)
+#  bash setup.sh
+#  best in cloud 9 to just right click this file and select run
+
+
+
+
+
+a02-rocksetta-gcc-java8.sh
+
+a03-rocksetta-bazel.sh
+
+a04-rocksetta-android-sdk.sh
+
+
+a06-rocksetta-setup-android.sh
+
+
+a07-rocksetta-gradle.sh
+
+
+a08-rocksetta-paths.sh
+
+a01-rocksetta-checks.sh
