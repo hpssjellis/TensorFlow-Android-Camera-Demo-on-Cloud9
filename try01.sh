901e6fdbe332cab72a0cdf09d47a3855633dd317
Jeremy Ellis <keyfreemusic@gmail.com>
2016-01-23 06:53:04 -0800

    Create try01.sh

---
+++ try01.sh
@@ -0,0 +1,29 @@
+#!/bin/bash  
+
+#  only need to run this script with the command (do not type the #)
+#  bash a10.rocksetta-create.sh
+# best in cloud 9 to just right click this file and select run
+
+
+
+android create project \
+--target 1 \
+--name MyAndroidApp \
+--path dddddddddd \
+--activity MyAndroidAppActivity \
+--package com.example.myandroid
+
+# --target android-20
+
+
+
+
+#from a terminal inside a new folder
+#android create project --name YourFredProject --path . --target android-20 --package tld.yourdomain.YourProject --activity YourFredProject
+
+
+#android update project --target <your build target> --path <path to LunarLander example>
+
+
+
+#android update project --target android-20 --path dddddddddd
