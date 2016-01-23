ac01dbbc3fcc834f2baf4ab6043b7498a058f4dd
Jeremy Ellis <keyfreemusic@gmail.com>
2016-01-22 16:10:37 -0800

    Create a10.rocksetta-create.sh

---
+++ a10.rocksetta-create.sh
@@ -0,0 +1,14 @@
+#!/bin/bash  
+
+#  only need to run this script with the command (do not type the #)
+#  bash a10.rocksetta-create.sh
+# best in cloud 9 to just right click this file and select run
+
+
+
+android create project \
+--target 1 \
+--name MyAndroidApp \
+--path wow \
+--activity MyAndroidAppActivity \
+--package com.example.myandroid
