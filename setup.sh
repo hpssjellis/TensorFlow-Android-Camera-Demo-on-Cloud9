9aeda9f0b3fc33c2ab3a4b875c1efd234da246ef
Jeremy Ellis <keyfreemusic@gmail.com>
2016-01-23 14:21:55 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -26,3 +26,11 @@ bash a08-rocksetta-paths.sh
 bash a09-rocksetta-tensorflow.sh
 
 bash a01-rocksetta-checks.sh
+
+
+echo "Now lets make an App. Checkout a10.rocksetta-create.sh to see the steps"
+
+bash a10.rocksetta-create.sh
+
+echo "Look in the wow3/bin folder to see your myNameWow3-debug.apk file"
+echo "run the index.html file and then Preview preview running App to view the .apk on a webpage to test on your Android 4.4.0 phone"
