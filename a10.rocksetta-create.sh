c2b40747d14ccc041815de0fb293306807f30577
Jeremy Ellis <keyfreemusic@gmail.com>
2016-01-23 14:10:50 -0800

    Update a10.rocksetta-create.sh

--- a10.rocksetta-create.sh
+++ a10.rocksetta-create.sh
@@ -7,8 +7,24 @@
 
 
 android create project \
---target 1 \
---name MyAndroidApp \
---path wow \
---activity MyAndroidAppActivity \
---package com.example.myandroid
+--target android-20 \
+--name myNameWow3 \
+--path wow3 \
+--activity myActivityWow3 \
+--package com.example.wow3
+
+
+
+#cd wow2
+
+#android update project --path .
+
+#ant
+
+android update project --name myNameWow3 --target android-20 --path wow3
+
+cd wow3
+
+ant clean
+
+ant debug
